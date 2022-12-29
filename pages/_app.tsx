817844a3799c97339926c7a1e892b131959ca6ab
Yike Gao <yikegao8@gmail.com>
2022-12-29 21:20:54 +0000

    12.29

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
+const MyApp=({ Component, pageProps }: AppProps) =>{
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(()=>{
+    setIsSSR(false)
+  },[])
+  if(isSSR) return null;
+  return
+  (<div>
+      
+      <div className='flex gap-6 md:gap-20'>
+    <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    </div>
+      </div>
+
+  <Component {...pageProps} />
+    </div>
+  )
+
 }
+export default MyApp
