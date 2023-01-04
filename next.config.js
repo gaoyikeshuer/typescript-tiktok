e28d337523a1ae5023bb6abda58d110f2823b296
Yike Gao <yikegao8@gmail.com>
2023-01-04 16:07:45 +0000

    1.4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['lwlies.com'],//this going to allow next.js render images
+  }
 }
 
 module.exports = nextConfig
