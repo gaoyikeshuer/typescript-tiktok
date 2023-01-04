828e77daef126592cb5bf4ff2f7a47c2d6ab6ab9
Yike Gao <yikegao8@gmail.com>
2023-01-04 13:12:05 +0000

    1.4

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,8 +4,13 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
 import {ImCancelCircle} from 'react-icons/im'
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 const Sidebar = () => {
     const[showSidebar, setShowSidebar] = useState(true)
+    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded'
+    const [userProfile, setUserProfile] = useState(false)
   return (
     <div>
       <div className='block xl:hidden m-2 ml-4 mt-3 text-xl'
@@ -14,17 +19,34 @@ const Sidebar = () => {
      {showSidebar?<ImCancelCircle/>:<AiOutlineMenu/>}
       </div>
       {showSidebar && (
-        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
+        <div className='w-20 xl:w-80 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
         <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
        <Link href="/">
-        <div>
+        <div className={normalLink}>
             <p className='text-2xl'>
                 <AiFillHome/>
             </p>
+            <span className='text-xl hidden xl:block'>
+             For you
+            </span>
         </div>
        </Link>
         </div>
+        {!userProfile && (
+            <div className='px-2 py-4 hidden xl:block'>
+                <p className='text-gray-400'>Log in to like and comment on video</p>
+                <div className='pr-4'>
+                 <button className='cursor-pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]'>
+                    Log in
+                 </button>
+                </div>
+            </div>
+        )}
+        <Discover/>
+        <SuggestedAccounts/>
+        <Footer/>
         </div>
+       
       )}
     </div>
   )
