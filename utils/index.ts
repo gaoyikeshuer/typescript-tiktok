7d20bb469d86547cf80371f7dcb78f81bffdb636
Yike Gao <yikegao8@gmail.com>
2023-01-05 22:41:50 +0000

    1.5

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import jwt_decode from 'jwt-decode'
 
 //where we fetch our google response
 
-export const createOrGetUser =async (response:any) => {
+export const createOrGetUser =async ({response}:{response:any}) => {
   const decoded =  jwt_decode(response.credential)
   console.log(decoded)
 }
