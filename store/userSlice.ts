5723ad8740a8d46c32deeb3cfd6196966f240438
Yike Gao <yikegao8@gmail.com>
2023-01-08 14:45:26 +0000

    1.6

---
+++ store/userSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+
+interface UserState{
+
+  name: string
+}
+const initialState: UserState={
+    name:""
+}
+
+export const userSlice =createSlice({
+    name:'user',
+    initialState,
+    reducers:{
+      addUser:(state, action:PayloadAction<string>)=>{
+        state.name = action.payload
+      }
+    }
+})
+
+export const {addUser} = userSlice.actions
+export const selectCount = (state: RootState) => state.user.name
+export default userSlice.reducer
